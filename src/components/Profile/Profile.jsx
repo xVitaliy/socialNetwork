46fdffc602a58ecf89f0808b8b01f06a4a018553
Ivashin Vitaliy <ivasinvitaliy1989@gmail.com>
2021-10-23 18:12:04 +0300

    18 page Dialogs

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,7 +3,7 @@ import MyPosts from "./MyPosts/MyPosts";
 
 const Profile = () => {
     return (
-        <div className={s.content}>
+        <div>
             <div>
                 <img src="https://p4.wallpaperbetter.com/wallpaper/836/566/354/space-cosmo-hd-wallpaper-preview.jpg"
                      alt=""/>
