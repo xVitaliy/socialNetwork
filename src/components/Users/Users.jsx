25803ca801428d13214396ea52945d5b205af0de
Ivashin Vitaliy <ivasinvitaliy1989@gmail.com>
2021-10-30 21:13:30 +0300

    54 life cycle

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -31,11 +31,12 @@ import React, { Component } from "react";
 // ])
 
 class Users extends React.Component {
-    constructor(props) {
-        super(props);
+
+    componentDidMount() {
         axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
             this.props.setUsers(response.data.items)
         })
+
     }
 
     render() {
