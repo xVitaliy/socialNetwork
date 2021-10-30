d8a846209b783906fd7f2607eaab6f23ea887034
Ivashin Vitaliy <ivasinvitaliy1989@gmail.com>
2021-10-30 16:56:54 +0300

    53 extends React.Component

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,65 +1,76 @@
 import styles from './Users.module.css'
+import * as axios from "axios";
+import userPhoto from '../../assets/images/user.png'
+import React, { Component } from "react";
 
-const Users = (props) => {
-    
-    if (props.users.length === 0) {
-        props.setUsers([
-            {
-                id: 1,
-                photoUrl: 'https://www.kindpng.com/picc/m/78-786678_avatar-hd-png-download.png',
-                followed: false,
-                fullName: 'Vitaliy',
-                status: 'I am a boss',
-                location: { city: 'Alchevsk', country: 'Ukraine' }
-            },
-            {
-                id: 2,
-                photoUrl: 'https://www.kindpng.com/picc/m/78-786678_avatar-hd-png-download.png',
-                followed: true,
-                fullName: 'Dmitry',
-                status: 'I am a boss',
-                location: { city: 'Minsk', country: 'Belarus' }
-            },
-            {
-                id: 3,
-                photoUrl: 'https://www.kindpng.com/picc/m/78-786678_avatar-hd-png-download.png',
-                followed: false,
-                fullName: 'Andrew',
-                status: 'I am a boss',
-                location: { city: 'Kiev', country: 'Ukraine' }
-            },
-        ])
+// arrayUsers([
+//     {
+//         id: 1,
+//         photoUrl: 'https://www.kindpng.com/picc/m/78-786678_avatar-hd-png-download.png',
+//         followed: false,
+//         fullName: 'Vitaliy',
+//         status: 'I am a boss',
+//         location: { city: 'Alchevsk', country: 'Ukraine' }
+//     },
+//     {
+//         id: 2,
+//         photoUrl: 'https://www.kindpng.com/picc/m/78-786678_avatar-hd-png-download.png',
+//         followed: true,
+//         fullName: 'Dmitry',
+//         status: 'I am a boss',
+//         location: { city: 'Minsk', country: 'Belarus' }
+//     },
+//     {
+//         id: 3,
+//         photoUrl: 'https://www.kindpng.com/picc/m/78-786678_avatar-hd-png-download.png',
+//         followed: false,
+//         fullName: 'Andrew',
+//         status: 'I am a boss',
+//         location: { city: 'Kiev', country: 'Ukraine' }
+//     },
+// ])
+
+class Users extends React.Component {
+    constructor(props) {
+        super(props);
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+            this.props.setUsers(response.data.items)
+        })
     }
 
-    return (
-        <div>
-            {
-                props.users.map(u => <div key={u.id}>
+    render() {
+        return (
+            <div>
+                {
+                    this.props.users.map(u => <div key={u.id}>
                     <span>
                         <div>
-                            <img className={styles.userPhoto} src={u.photoUrl}/>
+                            <img className={styles.userPhoto}
+                                 src={u.photos.small !== null ? u.photos.small : userPhoto}/>
                         </div>
                         <div>
                             {u.followed
-                                ? <button onClick={() => props.unfollow(u.id)}>UnFollow</button>
-                                : <button onClick={() => props.follow(u.id)}>Follow</button>}
+                                ? <button onClick={() => this.props.unfollow(u.id)}>UnFollow</button>
+                                : <button onClick={() => this.props.follow(u.id)}>Follow</button>}
 
                         </div>
                     </span>
-                    <span>
                         <span>
-                            <div>{u.fullName}</div>
+                        <span>
+                            <div>{u.name}</div>
                             <div>{u.status}</div>
                         </span>
                         <span>
-                            <div>{u.location.country}</div>
-                            <div>{u.location.city}</div>
+                            <div>{'u.location.country'}</div>
+                            <div>{'u.location.city'}</div>
                         </span>
                     </span>
-                </div>)
-            }
-        </div>
-    )
+                    </div>)
+                }
+            </div>
+        )
+    }
 }
+
 
 export default Users
